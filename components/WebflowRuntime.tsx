07680fd56642f2f26d94999a657e22f264959cf0
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-20 06:21:24 -0700

    Add Webflow client runtime

---
+++ components/WebflowRuntime.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Script from "next/script";
+
+const SITE_ID = "6883334a2c2e5c0d3344a5e4";
+
+export function WebflowRuntime() {
+  return (
+    <>
+      <Script id="webflow-document-classes" strategy="beforeInteractive">
+        {`(function(w,d){var n=d.documentElement,t=' w-mod-';n.className+=t+'js';if('ontouchstart' in w||(w.DocumentTouch&&d instanceof DocumentTouch)){n.className+=t+'touch';}})(window,document);`}
+      </Script>
+      <Script
+        src={`https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8_site=${SITE_ID}.js`}
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.prod.website-files.com/6883334a2c2e5c0d3344a5e4/js/PSp0viuQwClK.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.prod.website-files.com/6883334a2c2e5c0d3344a5e4/js/8LKIzST87WPJ.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.prod.website-files.com/6883334a2c2e5c0d3344a5e4/js/j4KiEknB06Rx.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
+}
